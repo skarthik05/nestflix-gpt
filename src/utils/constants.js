996a8035920dc36f63789242844565b2c354f80e
karthik <karthiks0598@gmail.com>
2025-03-30 15:19:16 +0530

    chore(constant): add "language" selects

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,20 +8,12 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer " + 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwODgyYjg3MTdmMGI1NDkwMTQ2YmU3NGNhYzFjZmYwMyIsIm5iZiI6MTc0MzA5ODUzMi42ODEsInN1YiI6IjY3ZTU5MmE0NWYzZTBhYzE4ODAwMzVmOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4ZnXn4dD01n_lHdLq3tbRNnC3Dq83TuTdCO38b6e81c',
+    Authorization: "Bearer " + process.env.REACT_APP_TMDB_API_TOKEN,
   },
 };
 
-export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
+export const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 
-export const BG_URL =
-  "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+export const OPENAI_API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
 
-export const SUPPORTED_LANGUAGES = [
-  { identifier: "en", name: "English" },
-  { identifier: "hindi", name: "Hindi" },
-  { identifier: "spanish", name: "Spanish" },
-];
-
-export const OPENAI_KEY = '0882b8717f0b5490146be74cac1cff03';
 
---
+++ src/utils/languageConstants.js
@@ -0,0 +1,53 @@
+export const SUPPORTED_LANGUAGES = [
+    {
+        identifier: "en",
+        name: "English",
+    },
+    {
+        identifier: "hindi",
+        name: "Hindi",
+    },
+    {
+        identifier: "kannada",
+        name: "Kannada",
+    },
+    {
+        identifier: "telugu",
+        name: "Telugu",
+    },
+    {
+        identifier: "tamil",
+        name: "Tamil",
+    },
+    {
+        identifier: "marathi",
+        name: "Marathi",
+    },
+
+]
+export const lang = {
+    en: {
+        search: "Search",
+        searchPlaceholder: "Search for movies or TV shows",
+    },
+    hindi: {
+        search: "खोज",
+        searchPlaceholder: "फिल्में या टीवी शों के लिए खोजें",
+    },
+    kannada: {
+        search: "ಹುಡುಗಿಸಿ",
+        searchPlaceholder: "ಫಿಲ್ಮ್ಸ್ ಅಥವಾ ಟೀವಿ ಶೋಂ ಕ್ಕೆ ಹುಡುಗಿಸಿ",
+    },
+    telugu: {
+        search: "వెతికించు",
+        searchPlaceholder: "ఫీల్మ్స్ లేదా టీవీ శోంలు క್కೆ హುడುగిసి",
+    },
+    tamil: {
+        search: "தேடு",
+        searchPlaceholder: "படங்கள் அல்லது டிவி ஷோவ்ஸ் க்கு தேடு",
+    },
+    marathi: {
+        search: "खोज",
+        searchPlaceholder: "फिल्में या टीवी शों के लिए खोजें",
+    },
+}
